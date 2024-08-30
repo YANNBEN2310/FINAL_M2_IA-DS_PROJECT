58b8ef42b5d0c8f80acbd63758cdfd49d83fbcaf
Nacer BEN ABDERRAHMANE <n.benabderrahmane@EDEV.local>
2024-08-30 16:34:54 +0200

    Description of the changes you made

--- static/js/home.js
+++ static/js/home.js
@@ -3,3 +3,27 @@ document.addEventListener('keydown', function(event) {
         window.location.href = '/summary';
     }
 });
+
+let touchstartX = 0;
+let touchendX = 0;
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        // Swipe left - go back to the previous page
+        window.history.back();
+    }
+
+    if (touchendX > touchstartX) {
+        // Swipe right - go to the summary page
+        window.location.href = '/summary';
+    }
+}
+
+document.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+}, false);
+
+document.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    handleGesture();
+}, false);
